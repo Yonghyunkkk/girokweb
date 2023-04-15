f84fa710c0552c0126adebb9971465b756221497
Yonghyun Kwon <yonghyunkwon98@gmail.com>
2023-04-15 18:03:18 +0800

    [Fix] Fix merge bug

--- frontend/src/components/Category/AddCategoryForm.tsx
+++ frontend/src/components/Category/AddCategoryForm.tsx
@@ -1,36 +1,32 @@
 import React, { useState } from "react";
 
 interface AddCategoryFormProps {
-    onAddCategory: (categoryPath: string[]) => void;
+  onAddCategory: (categoryPath: string[]) => void;
 }
 
 const AddCategoryForm: React.FC<AddCategoryFormProps> = ({ onAddCategory }) => {
-    const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
 
-        // Split the input value into an array of strings by "/"
-        const categoryPath = inputValue.split("/");
-        onAddCategory(categoryPath);
-        setInputValue("");
-    };
+    // Split the input value into an array of strings by "/"
+    const categoryPath = inputValue.split("/");
+    onAddCategory(categoryPath);
+    setInputValue("");
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={inputValue}
-                onChange={(event) => setInputValue(event.target.value)}
-<<<<<<< HEAD
-                placeholder="Enter new category (e.g. HKU/COMP3354)"
-=======
-                placeholder="Enter new category"
->>>>>>> cd479a708c80070b8c14c515622278563f89fd1e
-            />
-            <button type="submit">Add Category</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        placeholder="Enter new category (e.g. HKU/COMP3354)"
+      />
+      <button type="submit">Add Category</button>
+    </form>
+  );
 };
 
 export default AddCategoryForm;
